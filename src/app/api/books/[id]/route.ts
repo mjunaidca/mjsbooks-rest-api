0c1d229a770002e397e0a8445cfb345cfcea1d85
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2023-04-15 22:41:23 +0500

    Created dynamic book API route

---
+++ src/app/api/books/[id]/route.ts
@@ -0,0 +1,46 @@
+import { randomUUID } from "crypto";
+import { NextRequest, NextResponse } from "next/server";
+import postgres from "postgres";
+
+type Props = {
+    params: {
+        id: string;
+    }
+}
+
+export async function GET(request: NextRequest, { params: { id } }: Props) {
+
+    const conn = postgres({
+        ssl: require,
+    });
+    const result = await conn.unsafe
+        (`
+        SELECT 
+        books.id, 
+        books.name, 
+        books.author, 
+        books.isbn, 
+        book_types.type_name AS type,
+        books.price, 
+        books.current_stock, 
+        books.available
+    FROM books
+    JOIN book_types ON books.type_id = book_types.id
+    WHERE books.id = ${id}
+    
+        `);
+    // books.id, books.name, books.author, books.isbn, book_types.type_name as type, books.price, books.current_stock, books.available 
+    // JOIN book_types ON books.type_id = book_types.id
+    // WHERE book_types.type_name = '${t ype}'       
+
+    // console.log(result);
+
+    if (!result) {
+        throw new Error('Books Not Found')
+    }
+
+
+    return NextResponse.json(result)
+}
+
+

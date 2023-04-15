bf692e53de4d8c4b1ea3ddc77d33028aaf918698
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2023-04-15 22:12:00 +0500

    created status + books with optional params routes

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -1,17 +1,31 @@
+import { log } from "console";
 import { NextRequest, NextResponse } from "next/server";
 import postgres from "postgres";
 
 export async function GET(request: NextRequest) {
+
+    const { searchParams } = new URL(request.url);
+    const type = searchParams.get('type');
+    const limit = searchParams.get('limit');
+    // const type = Object.fromEntries(searchParams.entries());
+
+    console.log(type)
+
     const conn = postgres({
         ssl: require,
     });
     const result = await conn.unsafe
         (`
-        SELECT books.id, books.name, books.author, books.isbn, book_types.type_name as type, books.price, books.current_stock, books.available 
+        SELECT 
+        books.id, books.name, books.author, book_types.type_name as type, books.available 
         FROM books
-        JOIN book_types ON books.type_id = book_types.id;
-    `);
-    console.log(result);
+        JOIN book_types ON books.type_id = book_types.id
+        ${type ? `WHERE book_types.type_name = '${type}'` : ''}
+        ${limit ? `LIMIT ${limit}` : ''}
+        `);
+    // WHERE book_types.type_name = '${t ype}'       
+
+    // console.log(result);
 
     if (!result) {
         throw new Error('Books Not Found')
@@ -20,3 +34,5 @@ export async function GET(request: NextRequest) {
 
     return NextResponse.json(result)
 }
+
+

2f865db93950383baedacee597764736c79ac2cc
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2023-04-16 11:59:50 +0500

    simple frontend to present homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,15 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-12 sm:p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-12 sm:p-24 mb-6 sm:mb-0.5">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <p className="absolute left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Warrior: Muhammad Junaid Shaukat
         </p>
       </div>
 
       <div className="relative flex flex-col place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <h1 className="text-4xl pb-4 text-center md:text-6xl font-bold text-gray-900 mt-2 ">
+        <h1 className="text-4xl pb-4 text-center md:text-6xl font-bold text-gray-900 mt-20 lg:mt-4 ">
           Books Rest API
         </h1>
         <p className="  text-base text-center mx-auto  sm:text-lg md:text-xl text-gray-900 mt-4">
